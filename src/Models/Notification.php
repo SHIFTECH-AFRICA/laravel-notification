01d8758aab41755b92fea209e786d0ca05cb469a
techguy <dev.techguy@gmail.com>
2020-10-03 17:28:47 +0300

    :package: Updating compiled files or packages.

--- src/Models/Notification.php
+++ src/Models/Notification.php
@@ -2,6 +2,7 @@
 
 namespace Note\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -9,7 +10,7 @@ use Note\Uuids\Uuids;
 
 class Notification extends Model
 {
-    use Uuids, SoftDeletes;
+    use Uuids, SoftDeletes, HasFactory;
 
     /**
      * Indicates if the IDs are auto-incrementing.
